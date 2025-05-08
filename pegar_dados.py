a84bd45264f35eaa6fc4af0d9e7a2f21b7c847c9
João Vitor Pires Veloso <x561956@rede.sp>
2025-05-08 13:55:39 -0300

    COGEDI

---
+++ pegar_dados.py
@@ -0,0 +1,52 @@
+import base64
+from imap_tools import MailBox, AND
+from datetime import datetime
+
+def atualizar_dados():
+    email = 'cogedi.docsp@gmail.com'
+    senha = 'knwa wbvx lryz ybmc'
+    email_origem = 'diariooficial@prefeitura.sp.gov.br'
+
+    conteudo_email = None
+
+    hoje = datetime.now().date()
+
+    try:
+        mail = MailBox('imap.gmail.com').login(email, senha)
+
+        lista_emails = mail.fetch(AND(from_ = email_origem, date = hoje))
+        
+        if lista_emails:
+            for email in lista_emails:
+                conteudo_email = email.html
+                conteudo_email = conteudo_email.replace('background-image:url("https://www.prefeitura.sp.gov.br/cidade/secretarias/upload/governo/gestao/doc/bg.png")', '')
+                print(conteudo_email)
+                
+        if conteudo_email:
+
+            conteudo_email = base64.b64encode(conteudo_email.encode('utf-8')).decode('utf-8')
+            
+            conteudo_email = base64.b64decode(conteudo_email).decode('utf-8')
+
+            return conteudo_email
+        
+        else:
+            with open('templates/nao_recebido.html', 'r', encoding = 'utf-8') as file:
+                resposta_nao_recebido = file.read()
+            return resposta_nao_recebido
+        
+    except Exception as e:
+        print('Erro: ', e)
+
+        with open('templates/erro.html', 'r', encoding = 'utf-8') as file:
+            resposta_erro = file.read()
+        return resposta_erro
+    
+    finally:
+        mail.logout()
+
+
+if __name__ == '__main__':
+    atualizar_dados()
+
+#knwa wbvx lryz ybmc (app - smc.ticsmb@gmail.com) 
